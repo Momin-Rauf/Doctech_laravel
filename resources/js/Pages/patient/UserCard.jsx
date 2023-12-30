1870e9372744507233db06f03d842b2f0aef2824
waseep123 <zahidwaseem07@gmail.com>
2023-12-30 09:08:31 +0500

    feels a wholelot like christmas

--- resources/js/Pages/patient/UserCard.jsx
+++ resources/js/Pages/patient/UserCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const UserCard = ({ UserData, User }) => {
   return (
-    <div className="bg-[rgb(45,152,120)] flex flex-row h-[100vh] justify-between  items-center p-4 text-white">
+    <div className="">
+        <div className='' >
+      <img   alt="" />
+      
+      </div>
       <div>{User.map((p, index) => (
         <div key={index} className="mb-4">
           <p className="text-xl font-bold">{p.name}</p>
@@ -22,9 +27,7 @@ const UserCard = ({ UserData, User }) => {
           <p className="text-sm">Age: {p.age}</p>
         </div>
       ))}</div>
-      <div>image
-        Image
-      </div>
+    
     </div>
   );
 };
